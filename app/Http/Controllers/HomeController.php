bdb6c337d2d720d784158f5b3409b48b78ee7793
Ivan <ivanivakhov@gmail.com>
2024-04-27 15:29:30 +0300

    backup

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        if(Auth::attempt(['email' => 'test@test', 'password' => 'test'])) {
+            dd($request);
+
+        }
+        dd(auth()->check());
+    }
+}
