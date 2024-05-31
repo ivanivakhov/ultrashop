10a524423523a61ae0943f826777e3d31f3d617f
Ivan Ivakhov <ivanivakhov@gmail.com>
2024-05-31 12:56:20 +0300

    Add Brand and Category models

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'slug',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Brand $brand) {
+            $this->slug = $this->slug ?? str($brand->title)->slug();
+        });
+
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +22,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Brand::factory(20)->create();
+        Category::factory(20)->create();
     }
 }
