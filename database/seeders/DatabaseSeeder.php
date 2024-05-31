53dbf49a3ebe37de28d22e317976348d6fcfa3b5
Ivan Ivakhov <ivanivakhov@gmail.com>
2024-05-31 14:32:13 +0300

    Add product model with factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,6 +25,10 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         Brand::factory(20)->create();
-        Category::factory(20)->create();
+//        Category::factory(20)->create();
+
+        Product::factory(20)
+            ->has(Category::factory(3))
+            ->create();
     }
 }
