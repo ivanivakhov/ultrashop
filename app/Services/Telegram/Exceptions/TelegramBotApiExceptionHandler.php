2b895f4af0f8ca5cc5a43622536be31efbecf270
Ivan Ivakhov <ivanivakhov@gmail.com>
2024-06-18 22:34:52 +0300

    Create TelegramBotApiExceptionHandler

---
+++ app/Services/Telegram/Exceptions/TelegramBotApiExceptionHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services\Telegram\Exceptions;
+
+use Exception;
+
+final class TelegramBotApiExceptionHandler extends Exception
+{
+    public function report()
+    {
+
+    }
+
+    public function render()
+    {
+
+    }
+}
