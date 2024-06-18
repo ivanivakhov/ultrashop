87c16acaf73a8f7ca8aa8dc50faf612484f1dc2b
Ivan Ivakhov <ivanivakhov@gmail.com>
2024-06-18 22:34:36 +0300

    Wrap telegram exception with try\catch

--- app/Services/Telegram/TelegramBotAPI.php
+++ app/Services/Telegram/TelegramBotAPI.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Telegram;
 
+use App\Services\Telegram\Exceptions\TelegramBotApiExceptionHandler;
 use Illuminate\Support\Facades\Http;
 
 class TelegramBotAPI
@@ -10,9 +11,13 @@ class TelegramBotAPI
     public const HOST = 'https://api.telegram.org/bot';
     public static function sendMessage(string $token, int $chatId, string $text): void
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $text
-        ]);
+        try {
+            Http::get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chatId,
+                'text' => $text
+            ]);
+        } catch (TelegramBotApiExceptionHandler $e) {
+            throw new TelegramBotApiExceptionHandler($e->getMessage());
+        }
     }
 }
