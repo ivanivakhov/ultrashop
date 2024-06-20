3489936e824a012a864db7467919a450fdade2cd
Ivan Ivakhov <ivanivakhov@gmail.com>
2024-06-20 14:37:00 +0300

    Refactor models. Replace boot() method with trait

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\Models\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     use HasFactory;
+    use HasSlug;
 
     protected $fillable = [
         'slug',
@@ -19,16 +20,6 @@ class Product extends Model
         'price',
         'thumbnail'
     ];
-
-    protected static function boot(): void
-    {
-        parent::boot();
-
-        static::creating(function (Product $product) {
-            $product->slug = $product->slug ?? str($product->title)->slug();
-        });
-
-    }
 
     public function brand(): BelongsTo
     {
