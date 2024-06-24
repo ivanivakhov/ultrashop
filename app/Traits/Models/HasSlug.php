a1093eb68ea2d568d390a9fd749713c76f61481b
Ivan Ivakhov <ivanivakhov@gmail.com>
2024-06-24 21:04:18 +0300

    Refactor HasSlug.php unique slug

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -8,26 +8,58 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasSlug
 {
-    private static array $slugs = [];
-
     protected static function bootHasSlug(): void
     {
         static::creating(function (Model $model) {
-            $slug = str($model->{self::slugFrom()})->slug();
-
-            if (isset(static::$slugs[$slug->value()])) {
-                static::$slugs[$slug->value()]++;
-            } else {
-                static::$slugs[$slug->value()] = 0;
-            }
-
-            $suffix = static::$slugs[$slug->value()] === 0 ? "" : "-".static::$slugs[$slug->value()];
-            $model->slug = $model->slug ?? $slug . $suffix;
+            $model->makeSlug();
         });
+    }
+
+    protected function makeSlug(): void
+    {
+        if (!$this->{$this->slugColumn()}) {
+
+            $slug = $this->slugUnique(
+                str($this->{$this->slugFrom()})
+                    ->slug()
+                    ->value()
+            );
+
+            $this->{$this->slugColumn()} =  $slug;
+        }
+    }
+
+    protected function slugColumn(): string
+    {
+        return 'slug';
     }
 
     public static function slugFrom(): string
     {
         return 'title';
+    }
+
+    private function slugUnique(string $slug): string
+    {
+        $originalSlug = $slug;
+        $i = 0;
+
+        while($this->isSlugExists($slug)) {
+            $i++;
+
+            $slug = $originalSlug . '-' . $i;
+        }
+
+        return $slug;
+    }
+
+    private function isSlugExists(string $slug): bool
+    {
+        $query = $this->newQuery()
+            ->where(self::slugColumn(), $slug)
+            ->where($this->getKeyName(), '!=', $this->getKey())
+            ->withoutGlobalScopes();
+
+        return $query->exists();
     }
 }
